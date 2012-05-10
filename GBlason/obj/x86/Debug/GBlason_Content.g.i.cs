79ff981a3508b7e78e5874337899bc272f024d9d
Thomas Santonja <grathad@hotmail.fr>
2012-05-10 17:01:43 +0900

    Modification and finalization of the command management system. Time to go to the CoA Displayer.
    Signed-off-by: Thomas Santonja <grathad@hotmail.fr>

--- GBlason/obj/x86/Debug/GBlason_Content.g.i.cs
+++ GBlason/obj/x86/Debug/GBlason_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pictures/icons32/arrow_rotate_clockwise.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pictures/icons32/arrow_undo.png")]
 
 
